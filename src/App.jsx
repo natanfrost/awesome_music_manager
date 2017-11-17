1cb08d24685fff5817ba63820ce29a6e5415d872
Diamond <nataniel.r.v@gmail.com>
2017-11-17 11:41:53 -0200

    add start wars 'title like'

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,31 @@ class App extends Component{
     }
     
     search(){
-        console.log(this.state);
+        var token = 'BQDY22y0EsDfRbqM3g9-RiujhQ8Dpf_y-R3GGmSavZpm4gd0xOVJpquEp10_2QF5zi2GNYSfEBc9Er5CAOe0ie8SyAqHYaRHhaBHZgikHtgAiuNZZBS8UkVvcMkeRwY2AJiG7yHFV_3MoHh9OptBTioInAbI&refresh_token=AQAfGolzLLxt9V1eGBjnDDwaeMBPmG8y2E1umBW1xqjX8v90FHQIqMBCzEUGQk82oS3On-F_Ihp29_1cKfEdL1dVjuKxFLRIYZrRFeIfd8IgikAiCMnhXqFPvy_cCOXohUA'
+        const BASE_URL = 'https://api.spotify.com/v1/search?q='
+        const FETCH_URL = `${BASE_URL}${this.state.query}&type=artist&limit=1`;
+        fetch(FETCH_URL, {
+            method: 'GET',
+            headers:{
+                'Authorization': 'Bearer ' + token 
+            },
+            mode: 'cors',
+            cache: 'default'
+        })
+        .then(response => response.json())
+        .then(json => console.log(json));
     }
 
     render(){        
         return(
             <div>
                 <div className="title">Awesome Music Manager</div>
+                
                 <FormGroup>
                     <InputGroup>
                         <FormControl                             
                             type="text"
-                            placeholder="Search for a music..." 
+                            placeholder="Search for a music in a galaxy far far away..." 
                             onChange={e => {this.setState({query: e.target.value})}}
                             onKeyPress={e => {
                                 if(e.key === 'Enter'){
